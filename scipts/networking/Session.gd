477ea7be8328b366828c32227031acca48739327
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-08-19 20:20:22 +0200

    Implemented session exclusive rpc'ing

--- scipts/networking/Session.gd
+++ scipts/networking/Session.gd
@@ -18,17 +18,25 @@ func add_player_id(player_id: int):
 func has_player_id(player_id: int):
 	return player_ids.has(player_id)
 
-func rpc(callable: Callable, arg1=null, arg2=null):
-#	TODO: Fix errors / hackiness
-#	callable = callable.bindv(args) # (in Godot 4.2)
-
+func rpc_excluding(target_player_id: int, callable: Callable, arg1=null, arg2=null):
 	for player_id in player_ids:
-		if arg1 == null:
-			callable.rpc_id(player_id)
-		elif arg2 == null:
-			callable.rpc_id(player_id, arg1)
-		else:
-			callable.rpc_id(player_id, arg1, arg2)
+		if player_id != target_player_id:
+			rpc_id(player_id, callable, arg1, arg2)
+
+func rpc_id(player_id: int, callable: Callable, arg1=null, arg2=null):
+	#	TODO: Fix errors / hackiness
+	#	callable = callable.bindv(args) # (in Godot 4.2)
+
+	if arg1 == null:
+		callable.rpc_id(player_id)
+	elif arg2 == null:
+		callable.rpc_id(player_id, arg1)
+	else:
+		callable.rpc_id(player_id, arg1, arg2)
+
+func rpc(callable: Callable, arg1=null, arg2=null):
+	for player_id in player_ids:
+		rpc_id(player_id, callable, arg1, arg2)
 
 func _to_string() -> String:
 	return "Session(%s, %s, %s)" % ([turn] + player_ids)
